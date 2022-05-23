f8d0dfe8f0b471e2398d00183a390814b20995fa
BerenyiPeti <berenyipeti01@gmail.com>
2022-05-23 15:14:55 +0200

    tesztLoves asserten áltment

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -4,14 +4,14 @@ public class TorpedoTeszt {
 
     public static void main(String[] args) {
         System.out.println("Teszt");
-        
-        new TorpedoTeszt().tesztLoves(4);
-        
+
+        new TorpedoTeszt().tesztLovesTalalt(4);
+
         //assert 1 == 2 : "nem jó a találat ellenőrzése";
     }
 
-    public String tesztLoves(int poz) {
-        Hajo hajo = new Hajo();
+    public String tesztLovesTalalt(int poz) {
+        Hajo hajo = new Hajo(new int[]{2, 3, 4});
         String t = hajo.talalat(4);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return "";
